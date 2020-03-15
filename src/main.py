346346f7994d85135f8af493699addc7fa30c584
TrAyZeN <31016188+TrAyZeN@users.noreply.github.com>
2020-03-15 09:53:50 +0100

    Fix tree detection

--- src/main.py
+++ src/main.py
@@ -6,17 +6,13 @@ import axe
 import trees
 import screenshot as sc
 
-#image = cv2.imread("../data/" + sys.argv[1])
-#image = cv2.imread("../data/cache/image.png")
-
 while True:
 
-    frame = np.asarray(sc.screenshot(":D"))
+    frame = cv2.cvtColor(np.asarray(sc.screenshot(":D")), cv2.COLOR_RGB2BGR)
     axe.draw_axe_countours(frame, cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
     trees.draw_trees_contours(frame, cv2.cvtColor(frame, cv2.COLOR_BGR2HSV))
 
-    cv2.imshow("image", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
-    #cv2.imshow("image", frame)
+    cv2.imshow("image", frame)
 
     time.sleep(0.1)
 
--- src/trees.py
+++ src/trees.py
@@ -6,6 +6,7 @@ HSV_MIN_THRESH = np.array([43, 165, 101])
 HSV_MAX_THRESH = np.array([73, 176, 255])
 
 def _remove_grass_from_mask(mask, nb_components, stats, w, h):
+    print(w//53)
     for i in range(nb_components):
         if stats[i][2] < w//53:
             for y in range(stats[i][1], stats[i][1]+stats[i][3]+1):
@@ -21,6 +22,7 @@ def draw_trees_contours(image, hsv_image, color=(0, 255, 0)):
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(mask, 8, cv2.CV_32S)
     _remove_grass_from_mask(mask, nb_components, stats, w, h)
 
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(mask, 8, cv2.CV_32S)
     for i in range(nb_components):
         if stats[i][2] < w-20 and stats[i][3] < h-20:
             cv2.rectangle(image, (stats[i][0], stats[i][1]), (stats[i][0]+stats[i][2], stats[i][1]+stats[i][3]), color, 2)
